b55c12cfd23c645b41b72c8aca5a5ee9372309c7
Farmer_Markus <145863173+Farmer-Markus@users.noreply.github.com>
2023-12-23 00:01:46 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,90 +1,107 @@
 #!/bin/bash
 
-#offset: Die Stelle, an der die dwarfs Datei anfängt(in bytes). script.sh+dwarfs-universal-0.7.3-Linux-x86_64+1+atlauncher.tar.gz+2=offset
-scriptname=$(basename "$0")
+#Sets variables and functions#
+##############################
 scriptdir=$(cd $(dirname $0);pwd)
-mountpoint="/tmp"
+scriptname=$(basename "$0")
 offset=auto
-if [ -e $mountpoint/ATLauncher-Portable ]
-   then
+mountpoint="/tmp"
+athome=$scriptdir
+Begin_dwarfs_universal=`awk '/^#__Begin_dwarfs_universal__/ {print NR + 1; exit 0; }' $scriptdir/$scriptname`
+End_dwarfs_universal=`awk '/^#__End_dwarfs_universal__/ {print NR - 1; exit 0; }' $scriptdir/$scriptname`
 
-       mounted=1
+sh_mount () {
+  mkdir -p $mountpoint/ATLauncher-Portable/mount-tools
+  mkdir -p $mountpoint/ATLauncher-Portable/mnt
+  if [ "$mounted" == "" ]
+     then
 
-   else
-
-       mounted=0
+         awk "NR==$Begin_dwarfs_universal, NR==$End_dwarfs_universal" $scriptdir/$scriptname > $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 && chmod a+x $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64
+         $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 --tool=dwarfs $scriptdir/$scriptname $mountpoint/ATLauncher-Portable/mnt -o offset=$offset
 fi
+}
+
+sh_unmount () {
+  umount $mountpoint/ATLauncher-Portable/mnt
+  rm -r $mountpoint/ATLauncher-Portable
+  rm -f $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
+}
+
 sh_help () {
   echo 'sh Options                           Description'
   echo '----------                           -----------'
   echo '--use-internal-jar                   Uses internal ATLauncher.jar'
-  echo '                                     (you need to recompile sh for Updates,'
-  echo '                                     need to be used as first option!)'
+  echo '                                     (you need to recompile sh for Updates)'
   echo ''
-  echo '--mount                              Mounts the dwarfs filesystem in'
-  echo '                                     '$mountpoint''
+  echo '--mount                              Mounts the dwarfs filesystem in '$mountpoint''
+  echo '                                     (can be used with <--mountpoint>)'
   echo ''
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar /tmp/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --help
-  umount /tmp/ATLauncher-Portable/mnt
-  rm -r /tmp/ATLauncher-Portable
+  echo '--mountpoint=<path>                  Defines the mount location for the dwarfs'
+  echo '                                     image.(Default mountpoint: </tmp>)'
+  echo ''
+  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --help
+  umount $mountpoint/ATLauncher-Portable/mnt
+  rm -r $mountpoint/ATLauncher-Portable
   exit
 }
-Begin_dwarfs_universal=`awk '/^#__Begin_dwarfs_universal__/ {print NR + 1; exit 0; }' $scriptdir/$scriptname`
-End_dwarfs_universal=`awk '/^#__End_dwarfs_universal__/ {print NR - 1; exit 0; }' $scriptdir/$scriptname`
 
-mkdir -p $mountpoint/ATLauncher-Portable/mount-tools
-mkdir -p $mountpoint/ATLauncher-Portable/mnt
+sh_if_mounted () {
+  if [ ! -e $mountpoint/ATLauncher-Portable ]
+     then
 
-if [ "$mounted" == "0" ]
-   then
+         sh_mount
+         echo -e "\033[1;32mImage mounted\033[0;38m"
+         exit
 
-       awk "NR==$Begin_dwarfs_universal, NR==$End_dwarfs_universal" $scriptdir/$scriptname > /tmp/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 && chmod a+x /tmp/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64
-       $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 --tool=dwarfs $scriptdir/$scriptname $mountpoint/ATLauncher-Portable/mnt -o offset=$offset
+     else
+
+         umount $mountpoint/ATLauncher-Portable/mnt
+         rm -r $mountpoint/ATLauncher-Portable
+         echo -e "\033[1;31mImage unmounted\033[0;38m"
+         exit
 fi
+}
 
-if [ "$1" == "--help" ]; then sh_help; fi; if [ "$1" == "-?" ]; then sh_help; fi
+sh_internal_jar () {
+  echo -e "\033[1;32mUsing internal jar\033[0;38m"
+  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --working-dir $athome --no-launcher-update $atargs
+}
 
-if [ "$1" == "--mount" ]
-   then
+sh_external_jar () {
+  if [ ! -e $scriptdir/ATLauncher.jar ]
+     then
 
-       if [ "$mounted" == "0" ]
-          then
-
-              echo -e "\033[1;32mImage mounted\033[0;38m"
-              exit
-
-          else
-
-              umount /tmp/ATLauncher-Portable/mnt
-              rm -r /tmp/ATLauncher-Portable
-              echo -e "\033[1;31mImage unmounted\033[0;38m"
-              exit
+         cp $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar $scriptdir
 fi
-fi
+  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $scriptdir/ATLauncher.jar $atargs
+}
 
-mkdir -p $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/
-ln -s $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/ $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
-
-if [ "$1" == "--use-internal-jar" ]
-   then
-
-       echo -e "\033[1;32mUsing internal jar\033[0;38m"
-       $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar /tmp/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar $2 --working-dir $scriptdir --no-launcher-update
-
-   else
-
-       if [ ! -e $scriptdir/ATLauncher.jar ]
-          then
-
-              cp /tmp/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar $scriptdir
-fi
-
-       $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $scriptdir/ATLauncher.jar $1
-fi
-
-umount /tmp/ATLauncher-Portable/mnt
-rm -r /tmp/ATLauncher-Portable
-rm -f $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
+for i in "$@"
+do
+case $i in
+    -?|--h|--help)
+    sh_mount && sh_help
+    ;;
+    --mount)
+    mount=1
+    ;;
+    --use-internal-jar)
+    internaljar=1
+    ;;
+    --mountpoint=*)
+    mountpoint="${i#*=}"
+    ;;
+    *)
+    #atargs=$1
+    ;;
+esac
+done
+#Scriptstart#
+#############
+if [[ "$internaljar" == "1" && "$mount" = "1" ]]; then echo -e "\033[1;31mCan't use this options together!\033[0;38m"; fi
+if [[ "$internaljar" == "1" && "$mount" = "" ]]; then sh_mount && sh_internal_jar && sh_unmount; fi
+if [[ "$internaljar" == "" && "$mount" = "" ]]; then sh_mount && sh_external_jar && sh_unmount; fi
+if [[ "$internaljar" == "" && "$mount" = "1" ]]; then sh_if_mounted; fi
 
 exit
 #__Begin_dwarfs_universal__
