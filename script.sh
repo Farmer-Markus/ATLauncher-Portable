df057a84502b45e4d6c371670437288225dc122d
Farmer_Markus <145863173+Farmer-Markus@users.noreply.github.com>
2023-12-23 20:58:29 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,107 +1,14 @@
 #!/bin/bash
+Begin_squashfusetools_tar=`awk '/^#__Begin_squashfusetools_tar__/ {print NR + 1; exit 0; }' $0` #finds line number
+End_squashfusetools_tar=`awk '/^#__End_squashfusetools_tar__/ {print NR - 1; exit 0; }' $0`
 
-#Sets variables and functions#
-##############################
-scriptdir=$(cd $(dirname $0);pwd)
-scriptname=$(basename "$0")
-offset=auto
-mountpoint="/tmp"
-athome=$scriptdir
-Begin_dwarfs_universal=`awk '/^#__Begin_dwarfs_universal__/ {print NR + 1; exit 0; }' $scriptdir/$scriptname`
-End_dwarfs_universal=`awk '/^#__End_dwarfs_universal__/ {print NR - 1; exit 0; }' $scriptdir/$scriptname`
+Begin_java_runtime=`awk '/^#__Begin_java_runtime__/ {print NR + 1; exit 0; }' $0`
+End_java_runtime=`awk '/^#__End_java_runtime__/ {print NR - 1; exit 0; }' $0`
 
-sh_mount () {
-  mkdir -p $mountpoint/ATLauncher-Portable/mount-tools
-  mkdir -p $mountpoint/ATLauncher-Portable/mnt
-  if [ "$mounted" == "" ]
-     then
-
-         awk "NR==$Begin_dwarfs_universal, NR==$End_dwarfs_universal" $scriptdir/$scriptname > $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 && chmod a+x $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64
-         $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 --tool=dwarfs $scriptdir/$scriptname $mountpoint/ATLauncher-Portable/mnt -o offset=$offset
-fi
-}
-
-sh_unmount () {
-  umount $mountpoint/ATLauncher-Portable/mnt
-  rm -r $mountpoint/ATLauncher-Portable
-  rm -f $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
-}
-
-sh_help () {
-  echo 'sh Options                           Description'
-  echo '----------                           -----------'
-  echo '--use-internal-jar                   Uses internal ATLauncher.jar'
-  echo '                                     (you need to recompile sh for Updates)'
-  echo ''
-  echo '--mount                              Mounts the dwarfs filesystem in '$mountpoint''
-  echo '                                     (can be used with <--mountpoint>)'
-  echo ''
-  echo '--mountpoint=<path>                  Defines the mount location for the dwarfs'
-  echo '                                     image.(Default mountpoint: </tmp>)'
-  echo ''
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --help
-  umount $mountpoint/ATLauncher-Portable/mnt
-  rm -r $mountpoint/ATLauncher-Portable
-  exit
-}
-
-sh_if_mounted () {
-  if [ ! -e $mountpoint/ATLauncher-Portable ]
-     then
-
-         sh_mount
-         echo -e "\033[1;32mImage mounted\033[0;38m"
-         exit
-
-     else
-
-         umount $mountpoint/ATLauncher-Portable/mnt
-         rm -r $mountpoint/ATLauncher-Portable
-         echo -e "\033[1;31mImage unmounted\033[0;38m"
-         exit
-fi
-}
-
-sh_internal_jar () {
-  echo -e "\033[1;32mUsing internal jar\033[0;38m"
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --working-dir $athome --no-launcher-update $atargs
-}
-
-sh_external_jar () {
-  if [ ! -e $scriptdir/ATLauncher.jar ]
-     then
-
-         cp $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar $scriptdir
-fi
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $scriptdir/ATLauncher.jar $atargs
-}
-
-for i in "$@"
-do
-case $i in
-    -?|--h|--help)
-    sh_mount && sh_help
-    ;;
-    --mount)
-    mount=1
-    ;;
-    --use-internal-jar)
-    internaljar=1
-    ;;
-    --mountpoint=*)
-    mountpoint="${i#*=}"
-    ;;
-    *)
-    #atargs=$1
-    ;;
-esac
-done
-#Scriptstart#
-#############
-if [[ "$internaljar" == "1" && "$mount" = "1" ]]; then echo -e "\033[1;31mCan't use this options together!\033[0;38m"; fi
-if [[ "$internaljar" == "1" && "$mount" = "" ]]; then sh_mount && sh_internal_jar && sh_unmount; fi
-if [[ "$internaljar" == "" && "$mount" = "" ]]; then sh_mount && sh_external_jar && sh_unmount; fi
-if [[ "$internaljar" == "" && "$mount" = "1" ]]; then sh_if_mounted; fi
+awk "NR==$Begin_squashfusetools_tar, NR==$End_squashfusetools_tar" $0 | tar xzv -C .
+export LD_LIBRARY_PATH=/home/markus/Downloads/teest/libs
+awk "NR==$Begin_java_runtime, NR==$End_java_runtime" $0 > /home/markus/Downloads/teest/libs/squashfuse "{testsdfsd}" temp/
+#/home/markus/Downloads/teest/libs/squashfuse $test temp/
 
 exit
-#__Begin_dwarfs_universal__
+#__Begin_squashfusetools_tar__
