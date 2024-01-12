eb386487c2db366352072425d9704711b6a35d28
Farmer_Markus <145863173+Farmer-Markus@users.noreply.github.com>
2024-01-12 01:22:18 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -7,24 +7,26 @@ scriptname=$(basename "$0")
 offset=auto
 mountpoint="/tmp"
 workdir=$scriptdir
-Begin_dwarfs_universal=`awk '/^#__Begin_dwarfs_universal__/ {print NR + 1; exit 0; }' $scriptdir/$scriptname`
-End_dwarfs_universal=`awk '/^#__End_dwarfs_universal__/ {print NR - 1; exit 0; }' $scriptdir/$scriptname`
+workdiroption="--working-dir"
+Begin_dwarfs_universal=`awk '/^#__Begin_dwarfs_universal__/ {print NR + 1; exit 0; }' "$scriptdir/$scriptname"`
+End_dwarfs_universal=`awk '/^#__End_dwarfs_universal__/ {print NR - 1; exit 0; }' "$scriptdir/$scriptname"`
 
 sh_mount () {
   if [ ! -e "$mountpoint/ATLauncher-Portable" ]
      then
 
-         mkdir -p $mountpoint/ATLauncher-Portable/mount-tools
-         mkdir -p $mountpoint/ATLauncher-Portable/mnt 
-         awk "NR==$Begin_dwarfs_universal, NR==$End_dwarfs_universal" $scriptdir/$scriptname > $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 && chmod a+x $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64
-         $mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64 --tool=dwarfs $scriptdir/$scriptname $mountpoint/ATLauncher-Portable/mnt -o offset=$offset
+         mkdir -p "$mountpoint/ATLauncher-Portable/mount-tools"
+         mkdir -p "$mountpoint/ATLauncher-Portable/mnt" 
+         awk "NR==$Begin_dwarfs_universal, NR==$End_dwarfs_universal" "$scriptdir/$scriptname" > "$mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64" && chmod a+x "$mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64"
+         "$mountpoint/ATLauncher-Portable/mount-tools/dwarfs-universal-0.7.3-Linux-x86_64" --tool=dwarfs "$scriptdir/$scriptname" "$mountpoint/ATLauncher-Portable/mnt" -o offset=$offset
 fi
 }
 
 sh_unmount () {
-  umount $mountpoint/ATLauncher-Portable/mnt
-  rm -r $mountpoint/ATLauncher-Portable
-  rm -f $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
+  umount "$mountpoint/ATLauncher-Portable/mnt"
+  rm -r "$mountpoint/ATLauncher-Portable"
+  rm -f "$workdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma"
+  rm -r "$workdir/runtimes"
 }
 
 sh_help () {
@@ -36,30 +38,33 @@ sh_help () {
   echo '--mount                              Mounts the dwarfs filesystem in '$mountpoint''
   echo '                                     (can be used with <--mountpoint>)'
   echo ''
-  echo '--mountpoint=<full path>             Defines the mount location for the dwarfs'
+  echo '--mountpoint=<string>                Defines the mount location for the dwarfs'
   echo '                                     image.(Default mountpoint: </tmp>)'
   echo ''
   echo '--install                            Moves the image into your .local/share'
   echo '                                     folder and creates an desktop entry'
+  echo ''
+  echo '--working-dir=<string>               This forces the working directory for the'
+  echo '				     launcher.'
   echo '-------------------------------------------------------------------------------'
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --help
-  umount $mountpoint/ATLauncher-Portable/mnt
-  rm -r $mountpoint/ATLauncher-Portable
+  "$mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java" -jar "$mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar" --help | sed '/--working-dir/,+1 d' #removing the --working-dir option of the launcher help.(some issues with spaces in working-dir path)
+  umount "$mountpoint/ATLauncher-Portable/mnt"
+  rm -r "$mountpoint/ATLauncher-Portable"
   exit
 }
 
 sh_if_mounted () {
-  if [ ! -e $mountpoint/ATLauncher-Portable ]
+  if [ ! -e "$mountpoint/ATLauncher-Portable" ]
      then
 
          sh_mount
-         echo -e "\033[1;32mImage mounted in $mountpoint\033[0;38m"
+         echo -e "\033[1;32mImage mounted in "$mountpoint"\033[0;38m"
          exit
 
      else
 
-         umount $mountpoint/ATLauncher-Portable/mnt
-         rm -r $mountpoint/ATLauncher-Portable
+         umount "$mountpoint/ATLauncher-Portable/mnt"
+         rm -r "$mountpoint/ATLauncher-Portable"
          echo -e "\033[1;31mImage unmounted\033[0;38m"
          exit
 fi
@@ -67,20 +72,21 @@ fi
 
 sh_internal_jar () {
   echo -e "\033[1;32mUsing internal jar\033[0;38m"
-  mkdir -p $scriptdir/runtimes/minecraft/java-runtime-gamma/linux
-  ln -s $mountpoint/ATLauncher-Portable/mnt/java-runtime-17 $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar --no-launcher-update $workdir $atargs 
+  mkdir -p "$workdir/runtimes/minecraft/java-runtime-gamma/linux"
+  ln -s "$mountpoint/ATLauncher-Portable/mnt/java-runtime-17" "$workdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma"
+  "$mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java" -jar "$mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar" --no-launcher-update $workdiroption "$workdir" "$atargs"
 }
 
 sh_external_jar () {
-  if [ ! -e $scriptdir/ATLauncher.jar ]
+
+  if [ ! -e "$workdir/ATLauncher.jar" ]
      then
 
-         cp $mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar $scriptdir
+         cp "$mountpoint/ATLauncher-Portable/mnt/ATLauncher/ATLauncher.jar" "$workdir"
 fi
-  mkdir -p $scriptdir/runtimes/minecraft/java-runtime-gamma/linux
-  ln -s $mountpoint/ATLauncher-Portable/mnt/java-runtime-17 $scriptdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma
-  $mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java -jar $scriptdir/ATLauncher.jar $atargs
+  mkdir -p "$workdir/runtimes/minecraft/java-runtime-gamma/linux"
+  ln -s "$mountpoint/ATLauncher-Portable/mnt/java-runtime-17" "$workdir/runtimes/minecraft/java-runtime-gamma/linux/java-runtime-gamma"
+  "$mountpoint/ATLauncher-Portable/mnt/java-runtime-17/bin/java" -jar "$workdir/ATLauncher.jar" $workdiroption "$workdir" "$atargs"
 }
 
 sh_install () {
@@ -111,8 +117,8 @@ sh_install () {
          echo -e "\033[1;32mInstalling ATLauncher in ~/.local/share...\033[0;38m"
          sh_mount
          mkdir -p ~/.local/share/ATLauncher-Portable
-         cp $scriptdir/$scriptname ~/.local/share/ATLauncher-Portable
-         cp  $mountpoint/ATLauncher-Portable/mnt/install/ATLauncher.png ~/.local/share/ATLauncher-Portable/
+         cp "$scriptdir/$scriptname" ~/.local/share/ATLauncher-Portable
+         cp "$mountpoint/ATLauncher-Portable/mnt/install/ATLauncher.png" ~/.local/share/ATLauncher-Portable/
          if [ -e ~/.local/share/applications/atlauncher.desktop ]; then rm ~/.local/share/applications/atlauncher.desktop; fi
          echo -e "\033[1;32mCreating desktop entry...\033[0;38m"
          sh_create_entry
@@ -169,6 +175,9 @@ case $i in
     --mountpoint=*)
     mountpoint="${i#*=}"
     ;;
+    --working-dir=*)
+    workdir="${i#*=}"
+    ;;
     --install)
     install=1
     ;;
@@ -178,7 +187,6 @@ case $i in
 esac
 done
 
-if [ ! "$(echo $atargs | grep "working-dir")" == "" ]; then workdir=""; else workdir="--working-dir $workdir" ; fi
 if [ "$help" == "1" ]; then sh_mount && sh_help; fi
 if [ "$install" == "1" ]; then sh_install && exit; fi
 if [[ "$internaljar" == "1" && "$mount" = "1" ]]; then echo -e "\033[1;31mCan't use this options together!\033[0;38m"; fi
